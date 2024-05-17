c191541450f10d044e6859d26abcf97b42c3aa4a
Michał Bartczak <bartczak.programowanie@gmail.com>
2024-05-17 17:56:15 +0200

    created function for pagining and filter customer orders

--- src/main/java/com/magazyn/backendapi/dto/FilterOrderDTO.java
+++ src/main/java/com/magazyn/backendapi/dto/FilterOrderDTO.java
@@ -2,19 +2,20 @@ package com.magazyn.backendapi.dto;
 
 import com.magazyn.backendapi.entity.ShipmentDimensions;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 
 @Data
 @Component
+@Getter
 public class FilterOrderDTO {
     private String filterText;
     private LocalDate filterData;
-    private String status;
-    private ShipmentDimensions kindEur;
-    private ShipmentDimensions kindHp;
-    private int page = 0;
-    private int size = 10;
+    private String filterStatus;
+    private ShipmentDimensions filterKindEur;
+    private ShipmentDimensions filterKindHp;
+
 
 
 }
--- src/main/java/com/magazyn/backendapi/repository/CustomOrderRepositoryImpl.java
+++ src/main/java/com/magazyn/backendapi/repository/CustomOrderRepositoryImpl.java
@@ -17,7 +17,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Repository
 public class CustomOrderRepositoryImpl implements CustomOrderRepository {
     @PersistenceContext
@@ -25,82 +24,64 @@ public class CustomOrderRepositoryImpl implements CustomOrderRepository {
 
     @Override
     public List<Order> orderFilter(FilterOrderDTO filter) {
-
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Order> query = cb.createQuery(Order.class);
-        Root<Order> orderRoot = query.from(Order.class);
-
-        List<Predicate> predicates = new ArrayList<>();
-
-        // Filter by trackingNumber
-        if (filter.getFilterText() != null && !filter.getFilterText().matches(".*[a-zA-Z]+.*")) {
-            predicates.add(cb.like(orderRoot.get("trackingNumber"), "%" + filter.getFilterText() + "%"));
-        }
-
-        // Filter by date
-        if (filter.getFilterData() != null) {
-            predicates.add(cb.equal(orderRoot.get("creationDate"), filter.getFilterData()));
-        }
-
-        // Filter by status
-        if (!"WSZYSTKIE".equals(filter.getStatus())) {
-            OrderStatus status = OrderStatus.valueOf(filter.getStatus());
-            predicates.add(cb.equal(orderRoot.get("status"), status));
-        }
-
-        // Filter by kind
-        if (filter.getKindEur() == ShipmentDimensions.EUR && filter.getKindHp() != ShipmentDimensions.HP) {
-            predicates.add(cb.equal(orderRoot.get("dimensions"), ShipmentDimensions.EUR));
-        } else if (filter.getKindEur() != ShipmentDimensions.EUR && filter.getKindHp() == ShipmentDimensions.HP) {
-            predicates.add(cb.equal(orderRoot.get("dimensions"), ShipmentDimensions.HP));
-        }
-
-        query.where(predicates.toArray(new Predicate[0]));
-
-        // Sortowanie
-        Expression<Object> orderByCase = cb.selectCase()
-                .when(cb.equal(orderRoot.get("status"), OrderStatus.MAGAZYN), 1)
-                .when(cb.equal(orderRoot.get("status"), OrderStatus.DOSTAWA), 2)
-                .when(cb.equal(orderRoot.get("status"), OrderStatus.DOSTARCZONO), 3)
-                .otherwise(4);
-
-        query.orderBy(cb.asc(orderByCase));
-
-        return entityManager.createQuery(query).getResultList();
+//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Order> query = cb.createQuery(Order.class);
+//        Root<Order> orderRoot = query.from(Order.class);
+//
+//        List<Predicate> predicates = new ArrayList<>();
+//
+//        // Filter by trackingNumber
+//        if (filter.getFilterText() != null && !filter.getFilterText().matches(".*[a-zA-Z]+.*")) {
+//            predicates.add(cb.like(orderRoot.get("trackingNumber"), "%" + filter.getFilterText() + "%"));
+//        }
+//
+//        // Filter by date
+//        if (filter.getFilterData() != null) {
+//            predicates.add(cb.equal(orderRoot.get("creationDate"), filter.getFilterData()));
+//        }
+//
+//        // Filter by status
+//        if (filter.getFilterStatus() != null && !"WSZYSTKIE".equals(filter.getFilterStatus())) {
+//            OrderStatus status = OrderStatus.valueOf(filter.getFilterStatus());
+//            predicates.add(cb.equal(orderRoot.get("status"), status));
+//        }
+//
+//        // Filter by kind
+//        if (filter.getFilterKindEur() == ShipmentDimensions.EUR && filter.getFilterKindHp() != ShipmentDimensions.HP) {
+//            predicates.add(cb.equal(orderRoot.get("dimensions"), ShipmentDimensions.EUR));
+//        } else if (filter.getFilterKindEur() != ShipmentDimensions.EUR && filter.getFilterKindHp() == ShipmentDimensions.HP) {
+//            predicates.add(cb.equal(orderRoot.get("dimensions"), ShipmentDimensions.HP));
+//        }
+//
+//        query.where(predicates.toArray(new Predicate[0]));
+//
+//        // Sortowanie
+//        Expression<Object> orderByCase = cb.selectCase()
+//                .when(cb.equal(orderRoot.get("status"), OrderStatus.MAGAZYN), 1)
+//                .when(cb.equal(orderRoot.get("status"), OrderStatus.DOSTAWA), 2)
+//                .when(cb.equal(orderRoot.get("status"), OrderStatus.DOSTARCZONO), 3)
+//                .otherwise(4);
+//
+//        query.orderBy(cb.asc(orderByCase));
+//
+//        return entityManager.createQuery(query).getResultList();
+        return null;
     }
 
     @Override
     public Page<Order> orderFilterWithPagination(FilterOrderDTO filter, Pageable pageable) {
         System.out.println("Pageable Page: " + pageable.getPageNumber());
         System.out.println("Pageable Size: " + pageable.getPageSize());
+        System.out.println("KindEur: " + filter.getFilterKindEur());
+        System.out.println("KindHp: " + filter.getFilterKindHp());
+        System.out.println("Status: " + filter.getFilterStatus());
+        System.out.println("text: " + filter.getFilterText());
+        System.out.println("date: " + filter.getFilterData());
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Order> query = cb.createQuery(Order.class);
         Root<Order> orderRoot = query.from(Order.class);
-        List<Predicate> predicates = new ArrayList<>();
-        System.out.println("dodanie tracking");
-        // Filter by trackingNumber
-        if (filter.getFilterText() != null && !filter.getFilterText().matches(".*[a-zA-Z]+.*")) {
-            predicates.add(cb.like(orderRoot.get("trackingNumber"), "%" + filter.getFilterText() + "%"));
-        }
-        System.out.println("dodanie daty");
-        // Filter by date
-        if (filter.getFilterData() != null) {
-            predicates.add(cb.equal(orderRoot.get("creationDate"), filter.getFilterData()));
-        }
-        System.out.println("dodanie status");
-        // Filter by status
-        if (!"WSZYSTKIE".equals(filter.getStatus())) {
-            OrderStatus status = OrderStatus.valueOf(filter.getStatus());
-            predicates.add(cb.equal(orderRoot.get("status"), status));
-        }
-        System.out.println("dodanie rodzaj");
-        // Filter by kind
-        if (filter.getKindEur() == ShipmentDimensions.EUR && filter.getKindHp() != ShipmentDimensions.HP) {
-            predicates.add(cb.equal(orderRoot.get("dimensions"), ShipmentDimensions.EUR));
-        } else if (filter.getKindEur() != ShipmentDimensions.EUR && filter.getKindHp() == ShipmentDimensions.HP) {
-            predicates.add(cb.equal(orderRoot.get("dimensions"), ShipmentDimensions.HP));
-        }
+        List<Predicate> predicates = createPredicates(cb, orderRoot, filter);
 
         query.where(predicates.toArray(new Predicate[0]));
 
@@ -115,22 +96,27 @@ public class CustomOrderRepositoryImpl implements CustomOrderRepository {
 
         TypedQuery<Order> typedQuery = entityManager.createQuery(query);
 
-
         typedQuery.setFirstResult((int) pageable.getOffset());
         typedQuery.setMaxResults(pageable.getPageSize());
 
-        List<Order> orderList = typedQuery.getResultList();
-        Long count = 0L;  // inicjalizacja przed blokiem try-catch
+        List<Order> orderList;
+        try {
+            orderList = typedQuery.getResultList();
+        } catch (Exception e) {
+            System.out.println("Błąd podczas wykonywania zapytania: " + e.getMessage());
+            e.printStackTrace();
+            return new PageImpl<>(new ArrayList<>(), pageable, 0);
+        }
 
+        Long count = 0L;
         System.out.println("Wykonanie zapytania countQuery...");
         try {
+            // Użycie nowego `CriteriaQuery` do liczenia wyników z nowymi predykatami
             CriteriaQuery<Long> countQuery = cb.createQuery(Long.class);
-
-
             Root<Order> countRoot = countQuery.from(Order.class);
-
-            countQuery.select(cb.count(countRoot)).where(predicates.toArray(new Predicate[0]));
-            count = entityManager.createQuery(countQuery).getSingleResult();  // przypisanie wartości w bloku try
+            List<Predicate> countPredicates = createPredicates(cb, countRoot, filter);
+            countQuery.select(cb.count(countRoot)).where(countPredicates.toArray(new Predicate[0]));
+            count = entityManager.createQuery(countQuery).getSingleResult();
             System.out.println("Wynik zapytania countQuery: " + count);
         } catch (Exception e) {
             System.out.println("Błąd podczas wykonania zapytania countQuery: " + e.getMessage());
@@ -139,6 +125,38 @@ public class CustomOrderRepositoryImpl implements CustomOrderRepository {
 
         return new PageImpl<>(orderList, pageable, count);
     }
+
+    private List<Predicate> createPredicates(CriteriaBuilder cb, Root<Order> root, FilterOrderDTO filter) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        // Filter by trackingNumber
+        if (filter.getFilterText() != null && !filter.getFilterText().matches(".*[a-zA-Z]+.*")) {
+            predicates.add(cb.like(root.get("trackingNumber"), "%" + filter.getFilterText() + "%"));
+        }
+
+        // Filter by date
+        if (filter.getFilterData() != null) {
+            predicates.add(cb.equal(root.get("creationDate"), filter.getFilterData()));
+        }
+
+        // Filter by status
+        if (filter.getFilterStatus() != null && !"WSZYSTKIE".equals(filter.getFilterStatus())) {
+            OrderStatus status = OrderStatus.valueOf(filter.getFilterStatus());
+            predicates.add(cb.equal(root.get("status"), status));
+        }
+
+        // Filter by kind
+        if (filter.getFilterKindEur() != null && ShipmentDimensions.EUR.equals(filter.getFilterKindEur()) &&
+                (filter.getFilterKindHp() == null || !ShipmentDimensions.HP.equals(filter.getFilterKindHp()))) {
+            predicates.add(cb.equal(root.get("dimensions"), ShipmentDimensions.EUR));
+        } else if (filter.getFilterKindHp() != null && ShipmentDimensions.HP.equals(filter.getFilterKindHp()) &&
+                (filter.getFilterKindEur() == null || !ShipmentDimensions.EUR.equals(filter.getFilterKindEur()))) {
+            predicates.add(cb.equal(root.get("dimensions"), ShipmentDimensions.HP));
+        }
+
+        return predicates;
+    }
+
 
     @Override
     public Long countOrdersByStatusForCurrentMonth(OrderStatus status) {
